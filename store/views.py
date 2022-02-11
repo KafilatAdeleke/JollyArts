89cfd846e9cd0e0ccb29eff4d04780aed2cf3aac
Kafilat Adeleke <adelekekafilatadenike@gmail.com>
2022-02-11 00:16:21 -0800

    first commit

---
+++ store/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import get_object_or_404, render
+
+from .models import Category, Product
+
+def product_all(request):
+    #products = Product.products.all()
+    return render(request, 'store/home.html')
