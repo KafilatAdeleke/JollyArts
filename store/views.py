56a7f164c307e29404028d5c9696d7ec7c1bc30f
Kafilat Adeleke <adelekekafilatadenike@gmail.com>
2022-02-12 05:35:04 -0800

    Added product pages

--- store/views.py
+++ store/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import get_object_or_404, render
 
 from .models import Category, Product
 
+
 def product_all(request):
-    #products = Product.products.all()
-    return render(request, 'store/home.html')
+    products = Product.products.all()
+    return render(request, 'store/home.html', {'products': products})
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, in_stock=True)
+    return render(request, 'store/products/single.html', {'product': product})
